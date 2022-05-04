a4465615902fba4d2516d34bbeb9465ccc22d3d3
Oriana11 <85575367+Oriana11@users.noreply.github.com>
2022-05-04 12:20:15 +0200

    Fixed some errors, and added javascript

--- Wave Animation/Javascript/script.js
+++ Wave Animation/Javascript/script.js
@@ -1,1 +1,8 @@
-const 
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('') //split the letters into an array
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`) //map it to create an array of letters with a span around them
+        .join('') //then we turn them back into a string
+})
